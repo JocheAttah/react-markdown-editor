d3dd963795f3fe7f3d1c2d4468986f99b546abcb
Attah David <jocheattahdavid@gmail.com>
2020-07-25 20:42:16 +0100

    setup and functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Reactmarkdown from 'react-markdown';
 import './App.css';
 
 function App() {
+
+  const [markdown, setMarkdown] = useState('# Supp')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea className='App__textarea' onChange={(e) => setMarkdown(e.target.value) } value={markdown} />
+
+      <Reactmarkdown  className='App__preview' source={markdown}/>
+      
     </div>
   );
 }
